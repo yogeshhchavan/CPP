fd3339317963ed23d10b79627595859393b25ef6
Yogesh Chavan <152694101+yogeshhchavan@users.noreply.github.com>
2025-11-11 12:50:52 +0530

    Update while.cpp

--- while.cpp
+++ while.cpp
@@ -13,6 +13,20 @@ int main(){
         cout<<count<<" ";
         count += 1;
     }
-    
+  
+ //To find the 1 to n odd number of sum....  
+  
+  int n,oddSum = 0;
+  cout<<"Enter the number :";
+  cin>>n;
+  int i =0;
+  while(i <= n){
+    if(i%2 != 0){
+      oddSum += i;
+    }
+    i++;
+  }
+  cout<<"Odd Sum :"<<oddSum;
+  
   return 0;
 }
