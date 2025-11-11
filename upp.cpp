2d7549218b584e36c1242d609c61dd8ad389a630
Yogesh Chavan <152694101+yogeshhchavan@users.noreply.github.com>
2025-11-11 11:03:57 +0530

    Create upp.cpp

---
+++ upp.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+using namespace std;
+
+int main(){
+  char ch;
+  cout<<"Enter the character ";
+  cin>>ch;
+
+  if(ch >='a' && ch <= 'z'){
+    cout<<"lowercase";
+  }
+  esle{
+    cout<<"uppercase";
+}
+  return 0;
+}
