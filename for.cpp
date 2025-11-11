ec3513b154df1d2c69dd522480c5b1c6cf55dee3
Yogesh Chavan <152694101+yogeshhchavan@users.noreply.github.com>
2025-11-11 12:11:39 +0530

    Implement for loop to sum numbers from 1 to n
    Fixes the for loop to correctly sum numbers from 1 to n.

---
+++ for.cpp
@@ -0,0 +1,15 @@
+#include<iostream.h>
+using namespace std;
+int main(){
+  // using for loop add the 1 to n number in it.......
+int sum,n;
+cout<<"Enter the number : ";
+cin>>n;
+
+for(i = 1; i <= n; i++){
+  sum += 1;
+  cout<<sum<<" ";
+}
+
+return 0;
+}  
