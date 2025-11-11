ace9f0c7afb770f0639ff84ba046589b0971f336
Yogesh Chavan <152694101+yogeshhchavan@users.noreply.github.com>
2025-11-11 11:43:52 +0530

    Create while.cpp

---
+++ while.cpp
@@ -0,0 +1,18 @@
+#include<iostream.h>
+using namespace std;
+int main(){
+  //while loop....
+
+    int n;
+    cout<<"Enter the number :";
+    cin>>n;
+    int count = 1;
+
+    while (count <= n)
+    {
+        cout<<count<<" ";
+        count += 1;
+    }
+    
+  return 0;
+}
