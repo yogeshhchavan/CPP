0674f65e71dd9ec6c0d4048dd608d04beb56dad9
Yogesh Chavan <152694101+yogeshhchavan@users.noreply.github.com>
2025-11-11 10:12:26 +0530

    Implement even/odd number checker

---
+++ even.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+  //Even / odd number print.....
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+    if (n%2==0){
+         cout<<"Even\n";
+         cout<<"Bye";
+     }
+    else{
+         cout<<"Odd";
+    }
+    return 0;
+}
