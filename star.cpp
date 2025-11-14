a8738395a69f00bd26753faf8ca222d97f78a6c1
Yogesh Chavan <152694101+yogeshhchavan@users.noreply.github.com>
2025-11-14 12:08:05 +0530

    Add star pattern printing in star.cpp
    Implement a program to print a star pattern.

---
+++ star.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n =4;
+    for(int i =1; i<=n; i++){// outer loop which print the line how many you want to print......
+        
+        for(int j=1; j<=n;j++){// inner loop which print how many pattern or data is showing to print.........
+            cout<<"* ";//  print the data...
+        }
+        cout<<endl;// print the data on next line ......
+    }
+    
+    return 0;
+}
+
+/* OUTPUT ----------->
+  * * * * 
+* * * * 
+* * * * 
+* * * *     */
