1948f61ad268565f0730cfaa4142d35c9068b221
Yogesh Chavan <152694101+yogeshhchavan@users.noreply.github.com>
2026-01-27 22:08:53 +0530

    Create tri.cpp

---
+++ tri.cpp
@@ -0,0 +1,20 @@
+// Triangle pattern using number...........
+#include<iostream>
+using namespasce std;
+int main(){
+int n = 6;
+  for(int i =0; i<n; i++){
+    for(int j =1; j<i+1; j++){
+      cout<<j<<" ";
+    }
+    cout<<endl;
+  return 0;
+}
+
+// output:::::::::::::::::::::::::
+   
+// 1 
+// 1 2 
+// 1 2 3 
+// 1 2 3 4 
+// 1 2 3 4 5
