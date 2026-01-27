81398169c1b4e094428f5ec2cad099d06892e8d8
Yogesh Chavan <152694101+yogeshhchavan@users.noreply.github.com>
2026-01-27 22:40:10 +0530

    Add Floyd's Triangle pattern in floyd_patt.cpp
    Implement Floyd's Triangle pattern printing.

---
+++ floyd_patt.cpp
@@ -0,0 +1,22 @@
+// Flod's Triangle pattern..............
+
+#include<iostream>
+using namespace std;
+int main(){
+  int n=4;
+  int mum =1;
+  for(int j =0; ji+1; j++){
+    cout<<num<<" ";
+   num = num+1;
+  }
+  cout<<endl;
+}
+  
+  return 0;
+}
+
+// // output...................
+// 1  
+// 2 3
+// 4 5 6
+// 7 8 9 10
